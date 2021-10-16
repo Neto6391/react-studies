ca22a814a20c002aa40ba02f530fae028dff1271
nettorodrigues <neto6391@gmail.com>
2021-10-16 00:07:48 -0300

    create a feature to create a favorite using context hook

--- demo-project/src/components/meetups/MeetupItem.js
+++ demo-project/src/components/meetups/MeetupItem.js
@@ -1,7 +1,28 @@
+import { useContext } from 'react';
+
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import FavoriteContext from '../../store/favorites-context';
 
 function MeetupItem(props) {
+    const favoriteContext = useContext(FavoriteContext);
+
+    const itemIsFavorite = favoriteContext.itemIsFavorite(props.id);
+
+    function toggleFavoriteStatusHandler() {
+        if (itemIsFavorite) {
+            favoriteContext.removeFavorite(props.id);
+        } else {
+            favoriteContext.addFavorite({
+                id: props.id,
+                title: props.title,
+                description: props.description,
+                image: props.image,
+                address: props.address
+            })
+        }
+    } 
+
     return (
         <li className={classes.item}>
           <Card>
@@ -14,7 +35,7 @@ function MeetupItem(props) {
               <p>{props.description}</p>
           </div>
           <div className={classes.actions}>
-              <button>To Favorites</button>
+              <button onClick={toggleFavoriteStatusHandler}>{ itemIsFavorite ? 'Remove from Favorites' : 'To Favorites' }</button>
           </div>
           </Card>
         </li>

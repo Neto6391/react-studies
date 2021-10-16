c8962f39a885e87ff9cc203f45ff9404b6cf712d
nettorodrigues <neto6391@gmail.com>
2021-10-15 22:59:54 -0300

    create component form to add new meetup

--- demo-project/src/pages/NewMeetup.js
+++ demo-project/src/pages/NewMeetup.js
@@ -1,5 +1,26 @@
-function NewMeetup() {
-    return (<div>NewMeetup Page</div>);
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+    const history = useHistory();
+
+    function onAddMeetupHandler(meetupData) {
+        fetch('https://react-getting-start-1603f-default-rtdb.firebaseio.com/meetups.json', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => history.replace('/'));
+    }
+
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
+        </section>
+    );
 }
 
-export default NewMeetup;
+export default NewMeetupPage;
